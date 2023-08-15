2e9a323c5ce0f8404b6594b17b30eb870cf06ffb
kasra <82941023+kasra28@users.noreply.github.com>
2023-08-15 03:43:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from os import system as s
-from time import sleep as sp
+
 
 s("clear")
 
@@ -16,60 +16,73 @@ class bcolors:
     UNDERLINE = '\033[4m'
     END = '\033[0m'
 
-print(bcolors.BOLD + "script write by <kasra akhavan> iranian hacker")
-sp(1)
-print(bcolors.YELLOW + "kasraakhavan401@gmail.com")
-sp(1)
-print(bcolors.RED + "this python code requires openvpn on your terminal and script tested on kali linux")
-sp(1)
-print(bcolors.DARKCYAN + "if you dont have openvpn install you can just type on your terminal sudo apt install openvpn")
-sp(2)
-print(bcolors.CYAN +'''
-Username: vpnbook
-Password: e7x76mc
-source is https://www.vpnbook.com/freevpn
-''')
-sp(2)
-menu = input(bcolors.GREEN + '''
-please enter which vpn did you want to use
-1)vpn for download ----> get blockd by google most off the time
-2)usa vpn ----> get blockd by google most off the time
-3)frence vpn -----> better than other but not good for download stufe
-<box>: ''')
 
-if menu == '1':
-    choose = input(bcolors.BLUE + "which server do you want to use:\n1)vpnbook-pl226-tcp80\n2)vpnbook-pl226-tcp443\n3)vpnbook-pl226-udp53\n4)vpnbook-pl226-udp25000\n<box>: ")
-    if choose == '1':
-        s("sudo openvpn vpn1/vpnbook-pl226-tcp80.ovpn")
-    elif choose == '2':
-        s("sudo openvpn vpn1/vpnbook-pl226-tcp443.ovpn")
-    elif choose == '3':
-        s("sudo openvpn vpn1/vpnbook-pl226-udp53.ovpn")
-    elif choose == '4':
-        s("sudo openvpn vpn1/vpnbook-pl226-udp25000.ovpn")
+def converter(i_url):
+    import requests
+    from io import BytesIO
+    from PIL import Image
+    import pytesseract
+
+    url = i_url
+    response = requests.get(url)
+    image = Image.open(BytesIO(response.content))
+    grayscale_image = image.convert("L")
+    text = pytesseract.image_to_string(grayscale_image)
+    return text
+
+def start():
+    print(bcolors.CYAN +f'''
+    Username: vpnbook
+    Password: {print(converter("https://www.vpnbook.com/password.php?t=0.69584000%201692080891"))}
+    source is https://www.vpnbook.com/freevpn
+    ''')
+    menu = input(bcolors.GREEN + '''
+    please enter which vpn did you want to use
+    1)vpn for download ----> get blockd by google most off the time
+    2)usa vpn ----> get blockd by google most off the time
+    3)frence vpn -----> better than other but not good for download stufe
+    <box>: ''')
+
+    if menu == '1':
+        choose = input(bcolors.BLUE + "which server do you want to use:\n1)vpnbook-pl226-tcp80\n2)vpnbook-pl226-tcp443\n3)vpnbook-pl226-udp53\n4)vpnbook-pl226-udp25000\n<box>: ")
+        if choose == '1':
+            s("sudo openvpn vpn1/vpnbook-pl226-tcp80.ovpn")
+        elif choose == '2':
+            s("sudo openvpn vpn1/vpnbook-pl226-tcp443.ovpn")
+        elif choose == '3':
+            s("sudo openvpn vpn1/vpnbook-pl226-udp53.ovpn")
+        elif choose == '4':
+            s("sudo openvpn vpn1/vpnbook-pl226-udp25000.ovpn")
+        else:
+            exit()
+    elif menu == '2':
+        choose = input(bcolors.BLUE + "which server do you want to use:\n1)vpnbook-us1-tcp80\n2)vpnbook-us1-tcp443\n3)vpnbook-us1-udp53\n4)vpnbook-us1-udp25000\n<box>: ")
+        if choose == '1':
+            s("sudo openvpn vpn2/vpnbook-us1-tcp80.ovpn")
+        elif choose == '2':
+            s("sudo openvpn vpn2/vpnbook-us1-tcp443.ovpn")
+        elif choose == '3':
+            s("sudo openvpn vpn2/vpnbook-us1-udp53.ovpn")
+        elif choose == '4':
+            s("sudo openvpn vpn2/vpnbook-us1-udp25000.ovpn")
+        else:
+            exit()
+    elif menu == '3':
+        choose = input(bcolors.BLUE + "which server do you want to use:\n1)vpnbook-fr1-tcp80\n2)vpnbook-fr1-tcp443\n3)vpnbook-fr1-udp53\n4)vpnbook-fr1-udp25000.ovpn\n<box>: ")
+        if choose == '1':
+            s("sudo openvpn vpn3/vpnbook-fr1-tcp80.ovpn")
+        elif choose == '2':
+            s("sudo openvpn vpn3/vpnbook-fr1-tcp443.ovpn")
+        elif choose == '3':
+            s("sudo openvpn vpn3/vpnbook-fr1-udp53.ovpn")
+        elif choose == "4":
+            s("sudo openvpn vpn3/vpnbook-fr1-udp25000.ovpn")
+        else:
+            exit()
     else:
-        exit()
-elif menu == '2':
-    choose = input(bcolors.BLUE + "which server do you want to use:\n1)vpnbook-us1-tcp80\n2)vpnbook-us1-tcp443\n3)vpnbook-us1-udp53\n4)vpnbook-us1-udp25000\n<box>: ")
-    if choose == '1':
-        s("sudo openvpn vpn2/vpnbook-us1-tcp80.ovpn")
-    elif choose == '2':
-        s("sudo openvpn vpn2/vpnbook-us1-tcp443.ovpn")
-    elif choose == '3':
-        s("sudo openvpn vpn2/vpnbook-us1-udp53.ovpn")
-    elif choose == '4':
-        s("sudo openvpn vpn2/vpnbook-us1-udp25000.ovpn")
-    else:
-        exit()
-elif menu == '3':
-    choose = input(bcolors.BLUE + "which server do you want to use:\n1)vpnbook-fr1-tcp80\n2)vpnbook-fr1-udp53\n3)vpnbook-fr1-udp25000\n<box>: ")
-    if choose == '1':
-        s("sudo openvpn vpn3/vpnbook-fr1-tcp80.ovpn")
-    elif choose == '2':
-        s("sudo openvpn vpn3/vpnbook-fr1-udp53.ovpn")
-    elif choose == '3':
-        s("sudo openvpn vpn3/vpnbook-fr1-udp25000.ovpn")
-    else:
-        exit()
-else:
-    print("some thing is wrong")
+        print("some thing is wrong")
+
+try:
+    start()
+except Exception :
+    print("error:something goes wrong please run the app again")
